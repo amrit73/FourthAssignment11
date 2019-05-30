26f08cd925dcb632e31fc231d62731f2d81debfc
amrit73 <amritxrestha75@gmail.com>
2019-05-30 11:48:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fourthassignment/Model/ImageModel.java
@@ -0,0 +1,17 @@
+package com.example.fourthassignment.Model;
+
+public class ImageModel {
+    private String image;
+
+    public ImageModel(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
